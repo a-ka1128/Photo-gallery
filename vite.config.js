3547947824d1afc3d60a569d60821fe179fd9ab9
a-ka1128 <acku165@gmail.com>
2026-02-01 17:15:19 +0900

    수정

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Photo-gallery/', // 리포지토리 이름을 적어줍니다. 앞뒤에 / 필수!
 })
